e522dec1177343630b195e8daec95b22c98341ee
Trần Đình Hoàng <hoangtd97@outlook.com>
2019-04-18 17:55:17 +0700

    [INIT]

---
+++ src/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function FuncAdapter({ func, thisArg, defaultArgs = [], before = [], after = [] }) {
+
+}
+
+function MethodAdapter({ Instance, Methods }) {
+
+};
+
+function PrototypeAdapter({ Class, Methods }) {
+
+}
+
+module.exports = { FuncAdapter, MethodAdapter, PrototypeAdapter };
